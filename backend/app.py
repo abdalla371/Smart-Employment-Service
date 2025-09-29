d340cda74b232bc00fbafdab946a01389cf58e79
abdalla371 <updallakeynan@gmail.com>
2025-09-29 19:57:38 +0300

    app.py

--- backend/app.py
+++ backend/app.py
@@ -270,7 +270,7 @@ def admin_users():
 # ---------------------------
 # Run App
 # ---------------------------
-if _name_ == "_main_":
+if __name__ == "__main__":
     with app.app_context():
         db.create_all()
     app.run(host="0.0.0.0", port=5000, debug=True)
