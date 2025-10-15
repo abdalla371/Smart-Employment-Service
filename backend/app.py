8ffa09268ec82ef6b62dac2f18d37a76614ececf
abdalla371 <updallakeynan@gmail.com>
2025-10-15 11:19:06 +0300

    app.py

--- backend/app.py
+++ backend/app.py
@@ -277,4 +277,6 @@ with app.app_context():
     db_sql.create_all()
 
 if __name__ == "__main__":
+    with app.app_context():
+    db_sql.create_all()
     app.run(host="0.0.0.0", port=5000, debug=True)
